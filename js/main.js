13753763c093d5ed3b265a3cecd7f4fb094245bb
Никита <1nik2121@gmail.com>
2022-10-15 20:55:44 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const getRandomNumber = (min, max, count = 0) => {
   }
 
   if (min > max) {
-    [min, max] = [max , min];
+    [min, max] = [max, min];
   }
 
   min = Math.ceil(min);
@@ -13,3 +13,71 @@ const getRandomNumber = (min, max, count = 0) => {
 };
 
 getRandomNumber();
+
+const typeArray = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkTimeArray = ['12:00', '13:00', '14:00'];
+const featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photosArray = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const offerArray = [];
+
+const buildArray = () => {
+
+  for (let i = 0; i < 10; i++) {
+
+    const getPhotoNum = () => {
+      let number = getRandomNumber(1, 10);
+
+      if (number < 10) {
+        number = `0${number}`;
+      }
+      return number;
+    };
+
+    const location = {
+      lat: getRandomNumber(35.65000, 35.70000, 5),
+      lng: getRandomNumber(139.70000, 139.80000, 5)
+    };
+
+    const getFeaturesArray = (curArray) => {
+      const lengthOfArray = getRandomNumber(1, curArray.length);
+      const array = [];
+
+      while (array.length < lengthOfArray) {
+        const indexOfEl = getRandomNumber(0, curArray.length - 1);
+        const el = curArray[indexOfEl];
+
+        if (!array.includes(el)) {
+          array.push(el);
+        }
+      }
+      return array;
+    };
+
+    const author = {
+      avatar: `img/avatars/user${getPhotoNum()}.png`,
+    };
+
+    const offer = {
+      title: 'Гостиница "у кекса"',
+      addres: `${location.lat} , ${location.lng}`,
+      price: getRandomNumber(1, 10000),
+      type: typeArray[getRandomNumber(1, typeArray.length - 1)],
+      rooms: getRandomNumber(1, 4),
+      guests: getRandomNumber(1, 10),
+      checkin: checkTimeArray[getRandomNumber(1, checkTimeArray.length - 1)],
+      checkout: checkTimeArray[getRandomNumber(1, checkTimeArray.length - 1)],
+      features: getFeaturesArray(featuresArray),
+      description: 'Лучшая гостиница!',
+      photos: getFeaturesArray(photosArray),
+    };
+
+    offerArray[i] = {
+      offer: offer,
+      author: author
+    };
+  }
+};
+
+
+buildArray();
+
